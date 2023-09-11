53007bc22018b849d1f7cc4054a92df3e6cc0d7f
Isaac Leong <isaacleong@Isaacs-MacBook-Pro.local>
2023-09-11 11:02:04 -0400

    Initial Setup for Hero Component

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas'
+import { EarthCanvas, BallCanvas, AvatarCanvas, StarsCanvas } from './canvas'
 import Hero from './Hero'
 import Navbar from './Navbar'
 import About from './About'
@@ -9,16 +9,16 @@ import Feedbacks from './Feedbacks'
 import Contact from './Contact'
 
 export {
-    Hero,
-    Navbar,
-    About,
-    Tech,
-    Experience,
-    Works,
-    Feedbacks,
-    Contact,
-    EarthCanvas,
-    BallCanvas,
-    ComputersCanvas,
-    StarsCanvas,
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  EarthCanvas,
+  BallCanvas,
+  AvatarCanvas,
+  StarsCanvas,
 }
