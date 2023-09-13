6a704932073272a821fe52cd9b0f451bc07d9fe0
Isaac Leong <isaacleong@Isaacs-MBP.fios-router.home>
2023-09-13 10:07:06 -0400

    Contact form

--- src/app/components/sections/contact/Contact.jsx
+++ src/app/components/sections/contact/Contact.jsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useRef } from 'react'
+import { useState, useId } from 'react'
 import { motion } from 'framer-motion'
 import emailjs from '@emailjs/browser'
 
@@ -8,89 +8,105 @@ import { EarthCanvas } from '../..'
 import { Section } from '../../layout'
 import { slideIn } from '../../../utils/motion'
 
-const Contact = () => {
-  const formRef = useRef(null)
+function createFormId(formControlId, controlName) {
+  return `${formControlId}-${controlName}`
+}
 
-  const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
-  })
-
+const ContactForm = () => {
   const [loading, setLoading] = useState(false)
+  const formControlId = useId()
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    const formData = new FormData(e.target)
+    await new Promise((resolve, reject) => setInterval(resolve, 2000))
+    setLoading(false)
+  }
 
   return (
-    <div className="xl:fles-row flex flex-col-reverse gap-10 overflow-hidden xl:mt-12">
+    <form onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
+      <div className="flex flex-col">
+        <label
+          htmlFor={createFormId(formControlId, 'name')}
+          className="mb-4 flex flex-row font-medium text-white"
+        >
+          Your Name
+        </label>
+        <input
+          id={createFormId(formControlId, 'name')}
+          type="text"
+          name="name"
+          placeholder="What`s your name?"
+          className="outlined-none rounded-lg border-none bg-tertiary px-6 py-4 font-medium text-white placeholder:text-secondary"
+          aria-required
+          required
+        />
+      </div>
+      <div className="flex flex-col">
+        <label
+          htmlFor={createFormId(formControlId, 'email')}
+          className="mb-4 flex flex-row font-medium text-white"
+        >
+          Your Email
+        </label>
+        <input
+          id={createFormId(formControlId, 'email')}
+          type="email"
+          name="email"
+          placeholder="What`s your email?"
+          className="outlined-none rounded-lg border-none bg-tertiary px-6 py-4 font-medium text-white placeholder:text-secondary"
+          required
+          aria-required
+        />
+      </div>
+      <div className="flex flex-col">
+        <label
+          htmlFor={createFormId(formControlId, 'message')}
+          className="mb-4 flex flex-row font-medium text-white"
+        >
+          Your Message
+        </label>
+        <textarea
+          id={createFormId(formControlId, 'message')}
+          rows={7}
+          name="message"
+          placeholder="What`s is your message for me?"
+          className="outlined-none rounded-lg border-none bg-tertiary px-6 py-4 font-medium text-white placeholder:text-secondary"
+        />
+      </div>
+      <div className="flex flex-col">
+        <button
+          type="submit"
+          className="shadow-md w-fit rounded-xl bg-tertiary px-8 py-3 font-bold text-white shadow-primary outline-none disabled:cursor-not-allowed"
+          disabled={loading}
+        >
+          {loading ? 'Sending...' : 'Send'}
+        </button>
+      </div>
+    </form>
+  )
+}
+
+const ContactSection = () => {
+  return (
+    <Section className="flex w-full flex-col-reverse gap-10 overflow-hidden xl:mt-12 xl:flex-row">
       <motion.div
         variants={slideIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] rounded-2xl bg-black-100 p-8"
       >
         <p className={`${styles.sectionSubText}`}>Get in touch</p>
         <h3 className={`${styles.sectionHeadText}`}>Contact.</h3>
-        <form
-          ref={formRef}
-          onSubmit={handleSubmit}
-          className="mt-12 flex flex-col gap-8"
-        >
-          <div className="flex flex-col">
-            <label
-              htmlFor="contact-name"
-              className="mb-4 flex flex-row font-medium text-white"
-            >
-              Your Name
-            </label>
-            <input
-              id="contant-name"
-              type="text"
-              name="name"
-              placeholder="What`s your name?"
-              className="outlined-none rounded-lg border-none bg-tertiary px-6 py-4 font-medium text-white placeholder:text-secondary"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label
-              htmlFor="contact-email"
-              className="mb-4 flex flex-row font-medium text-white"
-            >
-              Your Email
-            </label>
-            <input
-              id="contant-email"
-              type="email"
-              name="email"
-              placeholder="What`s your email?"
-              className="outlined-none rounded-lg border-none bg-tertiary px-6 py-4 font-medium text-white placeholder:text-secondary"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label
-              htmlFor="contact-name"
-              className="mb-4 flex flex-row font-medium text-white"
-            >
-              Your Message
-            </label>
-            <textarea
-              id="contant-message"
-              rows={7}
-              name="message"
-              placeholder="What`s is your message for me?"
-              className="outlined-none rounded-lg border-none bg-tertiary px-6 py-4 font-medium text-white placeholder:text-secondary"
-            />
-          </div>
-          <div className="flex flex-col">
-            <button
-              type="submit"
-              className="shadow-md w-fit bg-tertiary px-8 py-3 font-bold text-white shadow-primary outline-none"
-            >
-              {loading ? 'Sending...' : 'Send'}
-            </button>
-          </div>
-        </form>
+        <ContactForm />
       </motion.div>
-    </div>
+      <motion.dv
+        variants={slideIn('right', 'tween', 0.2, 1)}
+        className="h-[350px] md:h-[550px] xl:h-auto xl:flex-1"
+      >
+        <EarthCanvas />
+      </motion.dv>
+    </Section>
   )
 }
 
-export default Contact
+export default ContactSection
